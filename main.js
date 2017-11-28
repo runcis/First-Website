1534e41c329076a6955ba114cdb16eab10bf7e7a
Rinalds Pikse <rinalds1234@gmail.com>
2017-11-28 15:54:13 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+function headfun(){
+	document.getElementById('head').style.display = "none";
+}
+
+function more(){
+	document.getElementById('section-b').style.display = "block";
+	document.getElementById('body').style.height = "1350px";
+}
+
+function less(){
+	document.getElementById('section-b').style.display = "none";
+	document.getElementById('body').style.height = "1050px";
+}
+
+function dmore(){
+	document.getElementById('section-b').style.display = "none";
+	document.getElementById('body').style.height = "1050px";
+}
+
+function addDecision(){
+	document.getElementById('addDecision').style.display = "block";
+}
+
+function addDecisiontoList(){
+	document.getElementById('addDecision').style.display = "none";
+
+	var decision = document.getElementById('textOfaddDecision').value;
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(decision));
+	document.getElementById("list").appendChild(li);
+}
